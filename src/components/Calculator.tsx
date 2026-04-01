9642f7fc0fc0f786ad90882f42e7077d80ce9230
Regis <mucyoregis250@gmail.com>
2026-04-01 20:24:09 +0200

    Update the logic

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 const Calculator = () => {
-  const [rightSide, setRightSide] = useState("");
+  const [rightSide, setRightSide] = useState<string | number>("");
   const [track, setTrack] = useState("");
-  const [leftSide, setLeftSide] = useState("");
-  const [final, setFinal] = useState("");
+  const [leftSide, setLeftSide] = useState<string | number>("");
+  const [final, setFinal] = useState<string | number>("");
+  const [answer, setAnswer] = useState(false);
+  const [secondInput, setSecondInput] = useState(false);
+
   const handleRight = (e: React.MouseEvent<HTMLButtonElement>) => {
     const text = e.currentTarget.textContent;
 
@@ -14,27 +17,73 @@ const Calculator = () => {
     } else {
       setRightSide((prev) => `${prev}${text === "+" ? "" : text}`);
     }
-    if (track[track.length - 1] === "+") {
-      setFinal(rightSide + leftSide);
+  };
+
+  const handleSum = () => {
+    const first = Number(rightSide);
+    const secon = Number(leftSide);
+    setFinal(first + secon);
+  };
+
+  const checkFinal = () => {
+    if (final !== "") {
+      setAnswer(true);
     }
   };
 
-  // console.log(track);
-  console.log("The Right numbers:" + rightSide);
-  console.log("The left numbers:" + leftSide);
+  const second = () => {
+    if (leftSide !== "") setSecondInput(true);
+  };
   return (
     <>
       <section>
+        <p>This is the final answer : {answer ? final : rightSide} </p>
         <p>{`Right Side Numbers (${rightSide})`}</p>
         <p>{`Left Side Numbers (${leftSide})`}</p>
         <p>Tracking : {track}</p>
         <p>Final Answer : {final}</p>
         <section>
-          <button onClick={handleRight}>1</button>
-          <button onClick={handleRight}>2</button>
-          <button onClick={handleRight}>3</button>
-          <button onClick={handleRight}>+</button> <br />
-          <button>=</button>
+          <button
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleRight(e);
+              second();
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleRight(e);
+              second();
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleRight(e);
+            }}
+          >
+            3
+          </button>
+          <button
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleRight(e);
+              handleSum();
+              checkFinal();
+            }}
+          >
+            +
+          </button>{" "}
+          <br />
+          <button
+            onClick={() => {
+              handleSum();
+              second();
+            }}
+          >
+            =
+          </button>
         </section>
       </section>
     </>

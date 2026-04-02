a905f72422bfa375a757271289cbcc1ad32bef3a
Regis <mucyoregis250@gmail.com>
2026-04-02 23:19:14 +0200

    Enhance Calculator component with improved state handling and button functionality

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -4,29 +4,58 @@ const Calculator = () => {
   const [current, setCurrent] = useState(0);
   const [num, setNum] = useState<string>("");
   const [control, setControl] = useState(true);
+  const [firstNum, setFirstNum] = useState<string[]>([]);
+  const [secondNum, setSecondNum] = useState("");
+  const [calculation, setCalculation] = useState("");
+  const [trackNum, setTrackNum] = useState(false);
   const handleClicks = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const currentNum = e.currentTarget.textContent;
+    const signs = ["+", "-", "x", "/"];
+    const currentValue = e.currentTarget.textContent;
 
-    if (currentNum === "AC") {
+    if (currentValue === "AC") {
       setControl(true);
       setNum("");
       setCurrent(0);
+      setFirstNum([]);
+      setSecondNum("");
+    } else if (
+      currentValue === "+" ||
+      currentValue === "-" ||
+      currentValue === "/" ||
+      currentValue === "x"
+    ) {
+      // setCalculation(currentValue);
+      setTrackNum(true);
+      setFirstNum(num.split("").filter((e) => Number(e)));
+    } else if (trackNum) {
+      setSecondNum((prev) => prev.concat(currentValue));
     } else {
       setControl(false);
     }
-    setNum((prev) => prev.concat(currentNum));
+    setNum((prev) => prev.concat(currentValue));
   };
-  console.log(control);
-  console.log(num.split("").filter((e) => Number(e)));
+
+  // console.log(firstNum);
+  // console.log(secondNum.split("").filter((e) => Number(e)));
+  // console.log(secondNum.length === 0 ? true : false);
+  // // console.log(control);
+  // // console.log(num.split("").filter((e) => Number(e)));
+  // console.log(calculation);
+  // console.log(num.split("").filter((e) => Number(e)));
+  // console.log(trackNum);
+  // console.log(control);
+
   return (
     <>
       <section className="my-11">
         <main className="bg-gray-500 h-14 ">
-          {
-            <p className="text-right text-white text-4xl relative top-1.5 right-2.5">
-              {control ? current : num.split("").filter((e) => Number(e))}
-            </p>
-          }
+          <p className="text-right text-white text-4xl relative top-1.5 right-2.5">
+            {secondNum.length === 0
+              ? control
+                ? current
+                : num.split("").filter((e) => Number(e))
+              : secondNum.split("").filter((e) => Number(e))}
+          </p>
         </main>
         <section className="bg-gray-100 grid grid-cols-4 ">
           <button
@@ -44,10 +73,12 @@ const Calculator = () => {
             %
           </button>
           <button
-            onClick={() => {}}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleClicks(e);
+            }}
             className=" border border-gray-400 text-3xl font-medium bg-orange-500 text-white"
           >
-            &divide;
+            /
           </button>
           <button
             onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
@@ -74,7 +105,9 @@ const Calculator = () => {
             9
           </button>
           <button
-            onClick={() => {}}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleClicks(e);
+            }}
             className=" border border-gray-400 text-3xl font-medium bg-orange-500  text-white"
           >
             x
@@ -104,7 +137,9 @@ const Calculator = () => {
             6
           </button>
           <button
-            onClick={() => {}}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleClicks(e);
+            }}
             className=" border border-gray-400 text-3xl font-medium bg-orange-500  text-white"
           >
             -
